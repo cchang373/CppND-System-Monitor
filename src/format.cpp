1b87c8d56f435707f5fa5eed5eb43a87e70d9953
Chaoyi Chang <chaoyichang@ChaoyiChangsMBP.attlocal.net>
2022-07-12 11:05:03 -0400

    fixed foramt

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,23 @@
+#include "format.h"
+#include <iomanip>
 #include <string>
 
-#include "format.h"
-
 using std::string;
+using std::to_string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds [[maybe_unused]]) {
+  int h, m, s;
+  h = seconds / 3600;
+  m = (seconds % 3600) / 60;
+  s = seconds - 3600 * h - 60 * m;
+
+  std::ostringstream time;
+  time << std::setw(2) << std::setfill('0') << h << ":" << std::setw(2)
+       << std::setfill('0') << m << ":" << std::setw(2) << std::setfill('0')
+       << s;
+  return time.str();
+}
